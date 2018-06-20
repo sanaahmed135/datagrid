c2ddb5b0316597eec8c2a70aadf749f09820ce6b
Sana Saad <s.saad@solvin.com>
2018-06-20 15:21:27 +0200

    Column Format with Filter and Sort

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { productsGenerator } from '../node_modules/util';
-import './react-bootstrap-table2.min.css'
+import './react-bootstrap-table2.min.css';
+import filterFactory,{textFilter} from 'react-bootstrap-table2-filter'
 //import '../node_modules/react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 class App extends Component {
   
   render() {
-    // function rankFormatter(cell, row, rowIndex, formatExtraData) {
-    //   return (
-    //     <i className={ formatExtraData[cell] }></i>
-    //   );
-    // }
-    function priceFormatter(column,colIndex){
+
+    function priceFormatter(column,colIndex,{sortElement,filterElement}){
       return(
-        <h5><strong>$$ {column.text} $$</strong></h5>
+        <div style={{display:'flex',flexDirection:'column'}}>
+          { filterElement }
+          { column.text }
+          { sortElement }
+        </div>
       )
     }
     const products=[
@@ -33,68 +34,29 @@ class App extends Component {
         dataField: 'id',
         text: 'Product ID'
 
-        // attrs:{title:'id column'}
-        // align: 'center',
-        // style: {
-        //   fontWeight: 'bold',
-        //   fontSize: '18px'
-        // }
-        // events:{
-        //   onClick:()=> alert('Click on Product ID field')
-        // },
-        // classes:'demo-key-row'
+
       }, {
         dataField: 'name',
         text: 'Product Name',
-        // attrs: (cell,row,rowIndex,colIndex)=>({'data-test':`customized data ${rowIndex}`})
-        // title:(cell,row,rowIndex,colIndex)=>`this is custom title for ${cell}`,
-        // style: (cell, row, rowIndex, colIndex) => {
-        //   if (rowIndex % 2 === 0) {
-        //     return {
-        //       backgroundColor: '#81c784'
-        //     };
-        //   }
-        //   return {
-        //     backgroundColor: '#c8e6c9'
-        //   };
-        // }
 
-        // classes: (cell, row, rowIndex, colIndex) => {
-        //   if (rowIndex % 2 === 0) return 'demo-row-even';
-        //   return 'demo-row-odd';
-        // }
-      
-        // align:(cell,row,rowIndex,colIndex)=>{
-        //   if(rowIndex % 2 === 0) return 'right';
-        //   return 'left';
-        // }
       },
       {
         dataField:'price',
         text: 'Product Price',
+        sort: true,
+        filter: textFilter(),
         headerFormatter:priceFormatter
       }
-      // {
-      //   dataField: 'rank',
-      //   text: 'Rank',
-      //   formatter: rankFormatter,
-      //   formatExtraData: {
-      //     up: 'glyphicon glyphicon-chevron-up',
-      //     down: 'glyphicon glyphicon-chevron-down'
-      // }
-];
+    ]
       
     return (
       <div>
-        {/* <p>Cloud icon on a styled link button:
-          <a href="#" className="btn btn-info btn-lg">
-            <span className="glyphicon glyphicon-cloud"></span> Cloud 
-          </a>
-      </p>  */}
           <BootstrapTable
             keyField="id"
             data={ products }
             columns={ columns }
+            filter={ filterFactory() }
+
           />
       </div>
     );
