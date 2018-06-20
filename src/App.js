e0ebb13c14379e9c085cc0036257bb3da2aaa570
Sana Saad <s.saad@solvin.com>
2018-06-20 10:44:41 +0200

    Column Formatter with Custom Datat(error not resolved)

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './App.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import { productsGenerator } from '../node_modules/util';
+import './react-bootstrap-table2.min.css'
+//import '../node_modules/react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+class App extends Component {
+  
+  render() {
+    function rankFormatter(cell, row, rowIndex, formatExtraData) {
+    //  console.log( rowIndex)
+    //  console.log(formatExtraData[0]);
+      return (
+        <i className={ formatExtraData[cell] }></i>
+      );
+    }
+    const products=[
+      {
+        id:1, name:'Sana',rank:2100
+      },
+      {
+        id:2, name:'Saad',rank:2104
+      }
+    ];
+
+    const columns = [
+      // omit...
+      {
+        dataField: 'id',
+        text: 'Product ID'
+      }, {
+        dataField: 'name',
+        text: 'Product Name'
+      },
+      {
+        dataField: 'rank',
+        text: 'Rank',
+        formatter: rankFormatter,
+        formatExtraData: {
+          up: 'glyphicon glyphicon-chevron-up',
+          down: 'glyphicon glyphicon-chevron-down'
+      }
+      }];
+      
+    return (
+      <div>
+        {/* <p>Cloud icon on a styled link button:
+          <a href="#" className="btn btn-info btn-lg">
+            <span className="glyphicon glyphicon-cloud"></span> Cloud 
+          </a>
+      </p>  */}
+          <BootstrapTable
+            keyField="id"
+            data={ products }
+            columns={ columns }
+          />
+      </div>
+    );
+  }
+}
+
+export default App;
