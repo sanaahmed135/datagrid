6b197f59dc0e458d88331e45a8eaececd7c8e3bc
Sana Saad <s.saad@solvin.com>
2018-06-21 10:22:14 +0200

    Select Filter

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 import BootstrapTable from 'react-bootstrap-table-next';
-import { productsGenerator } from '../node_modules/util';
 import './react-bootstrap-table2.min.css';
-import filterFactory,{textFilter} from 'react-bootstrap-table2-filter'
-//import '../node_modules/react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import filterFactory, { selectFilter } from 'react-bootstrap-table2-filter';
 
-class App extends Component {
-  
+class App extends Component { 
   render() {
+    const selectOptions = {
+      0: 'Good',
+      1: 'Bad',
+      2: 'unknown'
+    };
 
-
-    const products=[
-      {
-        id:1, name:'Sana',price:2100
+    const products=[{
+        id:1, name:'Sana',quality:'Bad'
       },
       {
-        id:2, name:'Saad',price:2104
+        id:2, name:'Saad',quality:'Good'
+      },
+      {
+        id:3, name:'Sarah',quality:'unknown'
       }
     ];
 
-    const columns = [
-      // omit...
-      {
-        dataField: 'id',
-        text: 'Product ID'
-
-      }, {
-        dataField: 'name',
-        text: 'Product Name',
-        filter: textFilter({
-          caseSensitive: true
-
-        })
-
-      },
-      {
-        dataField:'price',
-        text: 'Product Price'
-      }
-    ]
+    const columns = [{
+      dataField: 'id',
+      text: 'Product ID'
+    }, {
+      dataField: 'name',
+      text: 'Product Name'
+    }, {
+      dataField: 'quality',
+      text: 'Product Quailty',
+      formatExtraData: (cell)=> selectOptions[cell],
+      //formatter: (cell)=> selectOptions[cell],
+      filter: selectFilter({
+        options: selectOptions
+      })
+    }];
       
     return (
       <div>
-          <BootstrapTable
-            keyField="id"
-            data={ products }
-            columns={ columns }
-            filter={filterFactory()}
-          />
+          <BootstrapTable keyField='id' 
+          data={ products } columns={ columns } 
+          filter={ filterFactory() } />
       </div>
     );
   }
